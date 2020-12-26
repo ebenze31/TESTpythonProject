307cd0d8117481f6c8432b5c0b5d61c218a3ebc5
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-26 20:23:51 +0700

    Update scraping_link.py

--- venv/scraping one2car/scraping_link.py
+++ venv/scraping one2car/scraping_link.py
@@ -17,7 +17,7 @@ def funcUrl_allpage():
     print("Connect")
 
 
-time = dt.datetime.now()
+    time = dt.datetime.now()
     print(time)
 
     url_home = 'https://www.one2car.com/%E0%B8%A3%E0%B8%96%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%AD%E0%B8%87-%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A-%E0%B8%82%E0%B8%B2%E0%B8%A2'
